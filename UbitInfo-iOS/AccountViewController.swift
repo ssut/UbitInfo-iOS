6c1b5237ec439504888739f9577995c399d38821
ssut <ssut@ssut.me>
2014-07-21 16:13:35 +0900

    Add login

---
+++ UbitInfo-iOS/AccountViewController.swift
@@ -0,0 +1,132 @@
+//
+//  AccountViewController.swift
+//  UbitInfo-iOS
+//
+//  Created by ssut on 7/20/14.
+//  Copyright (c) 2014 ssut. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: XLFormViewController {
+    var values: Dictionary<String, AnyObject> = Dictionary<String, AnyObject>()
+    var hud: MBProgressHUD = MBProgressHUD()
+    
+    override func viewDidLoad()  {
+        super.viewDidLoad()
+        
+        // HUD
+        self.hud = MBProgressHUD(view: self.view)
+        self.view.addSubview(hud)
+        
+        if HttpClient.instance.loggedIn {
+            drawUserView()
+        } else {
+            drawGuestView()
+        }
+    }
+    
+    func updateFormValues() {
+        let sectionCount: Int = self.form.formSections.count
+        for index in 0..<sectionCount {
+            var section = self.form.formSectionAtIndex(index)
+            if !section.isMultivaluedSection {
+                let rowCount: Int = section.formRows.count
+                for rowIndex in 0..<rowCount {
+                    var row: XLFormRowDescriptor = section.formRows[rowIndex] as XLFormRowDescriptor
+                    if row.tag != "" {
+                        let tag = row.tag as String
+                        self.values[tag] = row.value ? row.value : nil
+                    }
+                }
+            } else {
+                var multiValuedValuesArray: NSMutableArray = NSMutableArray()
+                let rowCount: Int = section.formRows.count
+                for rowIndex in 0..<rowCount {
+                    var row: XLFormRowDescriptor = section.formRows[rowIndex] as XLFormRowDescriptor
+                    multiValuedValuesArray.addObject(row.value)
+                }
+                let tag = section.multiValuedTag as String
+                self.values[tag] = multiValuedValuesArray
+            }
+        }
+    }
+    
+    func drawGuestView() {
+        var form: XLFormDescriptor = XLFormDescriptor.formDescriptorWithTitle("Login")
+        var section: XLFormSectionDescriptor = XLFormSectionDescriptor.formSection() as XLFormSectionDescriptor
+        var row: XLFormRowDescriptor
+        
+        form.addFormSection(section)
+        
+        row = XLFormRowDescriptor.formRowDescriptorWithTag("id", rowType: "text", title: "")
+        row.cellConfigAtConfigure.setObject("UbitInfo ID", forKey: "textField.placeholder")
+        section.addFormRow(row)
+        
+        row = XLFormRowDescriptor.formRowDescriptorWithTag("password", rowType: "password", title: "")
+        row.cellConfigAtConfigure.setObject("UbitInfo PW", forKey: "textField.placeholder")
+        section.addFormRow(row)
+        
+        self.form = form
+        
+        var button: UIBarButtonItem = UIBarButtonItem(title: "Login", style: UIBarButtonItemStyle.Plain, target: self, action: "login:")
+        self.navigationItem.rightBarButtonItem = button
+        
+        self.viewWillAppear(false)
+    }
+    
+    func drawUserView() {
+        var button: UIBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItemStyle.Plain, target: self, action: "logout:")
+        self.navigationItem.rightBarButtonItem = button
+        
+        self.viewWillAppear(false)
+    }
+    
+    func login(sender: AnyObject) {
+        updateFormValues()
+        
+        // check id field
+        if !self.values["id"] || (self.values["id"] as AnyObject? as? String) == "" {
+            var message = UIAlertView(title: "Error", message: "Please input Ubitinfo ID", delegate: self, cancelButtonTitle: "OK")
+            message.show()
+        }
+        
+        // check password field
+        else if !self.values["password"] || (self.values["password"] as AnyObject? as? String) == "" {
+            var message = UIAlertView(title: "Error", message: "Please input Ubitinfo PW", delegate: self, cancelButtonTitle: "OK")
+            message.show()
+        }
+
+        // login
+        else {
+            let userId = self.values["id"] as AnyObject? as? String
+            let userPass = self.values["password"] as AnyObject? as? String
+            
+            self.hud.show(true)
+            HttpClient.instance.login(userId!, userPass: userPass!, callback: {
+                (success: Bool, message: String) in
+                if !success {
+                    self.hud.mode = MBProgressHUDModeText
+                    self.hud.labelText = message
+                    self.hud.hide(true, afterDelay: 3)
+                } else {
+                    self.hud.customView = UIImageView(image: UIImage(named: "Checkmark.png"))
+                    self.hud.mode = MBProgressHUDModeCustomView
+                    self.hud.labelText = "Completed"
+                    self.hud.hide(true, afterDelay: 3)
+                    
+                    self.form = nil
+                    self.reloadFormRow(nil)
+                    self.tableView.reloadData()
+                    self.viewDidLoad()
+                    self.viewWillAppear(true)
+                    self.view.setNeedsDisplay()
+                }
+            })
+        }
+    }
+    
+    func logout(sender: AnyObject) {
+        
+    }
+}
--- UbitInfo-iOS/HttpClient.swift
+++ UbitInfo-iOS/HttpClient.swift
@@ -10,6 +10,16 @@ import Foundation
 let CLIENT_NAME: String = "UbitInfo-iOS"
 let URL_PREFIX: String = "http://ubit.info:3000"
 let URL_STATUS = URL_PREFIX + "/!/status"
+let URL_LOGIN = URL_PREFIX + "/!/user/entry"
+let URL_TOKEN = URL_PREFIX + "/!/token"
+
+let SUCCESS = ""
+let ERR_NETWORK = "Network error"
+let ERR_UNKNOWN = "Unknown error"
+let ERR_FETCH_TOKEN = "Failed to fetch access token"
+let ERR_INVALID_USER = "Username or password is incorrect"
+let ERR_NO_USER = "User not found"
+let ERR_INVALID_PASS = "Password is invalid"
 
 class HttpClient {
     struct Static {
@@ -23,10 +33,82 @@ class HttpClient {
     }
     
     var manager: AFHTTPRequestOperationManager = AFHTTPRequestOperationManager()
+    var loggedIn: Bool = false
     
     init() {
-        manager.requestSerializer = AFJSONRequestSerializer()
+//        manager.requestSerializer = AFJSONRequestSerializer()
         manager.requestSerializer.setValue(CLIENT_NAME, forHTTPHeaderField: "X-API-Host")
         manager.securityPolicy.allowInvalidCertificates = true
+    }
+    
+    func getToken(callback: (String) -> Void) {
+        HttpClient.instance.manager.GET(URL_TOKEN,
+            parameters: nil,
+            success: {
+                (operation: AFHTTPRequestOperation!, response: AnyObject!) in
+                var json = JSONValue(response)
+                var token = ""
+                if let t = json["token"].string {
+                    token = t
+                }
+                callback(token)
+                return Void()
+            },
+            failure: {
+                (operation: AFHTTPRequestOperation!, error: NSError!) in
+                println(error)
+                callback("")
+                return Void()
+            }
+        )
+        
+    }
+    
+    func login(userId: String, userPass: String, callback: (Bool, String) -> Void) {
+        getToken({ (token: String) in
+            if token == "" {
+                callback(false, ERR_FETCH_TOKEN)
+                return Void()
+            }
+            
+            var params = [
+                "authenticity_token": token,
+                "uid": userId,
+                "upw": userPass
+            ]
+            
+            HttpClient.instance.manager.POST(URL_LOGIN,
+                parameters: params,
+                success: {
+                    (operation: AFHTTPRequestOperation!, response: AnyObject!) in
+                    var json = JSONValue(response)
+                    var code = -1
+                    println(json)
+                    if let c = json["code"].integer {
+                        if c == 0 {
+                            HttpClient.instance.loggedIn = true
+                            callback(true, SUCCESS)
+                        } else if c == 1 {
+                            callback(false, ERR_NO_USER)
+                        } else if c == 2 {
+                            callback(false, ERR_INVALID_USER)
+                        } else if c == 3 {
+                            callback(false, ERR_INVALID_PASS)
+                        } else {
+                            callback(false, ERR_UNKNOWN)
+                        }
+                    } else {
+                        callback(false, ERR_UNKNOWN)
+                    }
+                    return Void()
+                },
+                failure: {
+                    (operation: AFHTTPRequestOperation!, error: NSError!) in
+                    println(error)
+                    callback(false, ERR_NETWORK)
+                    return Void()
+                }
+            )
+        })
     }
 }
